88e16ee22d726acae6375d2b2d7e794ddccd788b
Anita <y_anita@mail.ru>
2019-10-14 11:40:15 +0300

    mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const keys = require('./keys');
 
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
+
+mongoose.connect(keys.mongoURI,  { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("MongoDB connected."))
+.catch((err) => console.error(err));
 
 const app = express();
 app.use(express.static(clientPath));
@@ -10,4 +16,3 @@ app.use(express.static(clientPath));
 app.listen(port, () => {
     console.log(`Server has been started on port: ${port}`);
 });
-
