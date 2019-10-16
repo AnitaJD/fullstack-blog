f543f971fe955d26e81ad75cf8e5974ec468b904
Anita <y_anita@mail.ru>
2019-10-16 12:14:04 +0300

    delete post

--- keys.js
+++ keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://anita:Iq3FB19mFN9CGpqI@cluster0-i1xvj.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://anita:@cluster0-i1xvj.mongodb.net/test?retryWrites=true&w=majority'
 }
